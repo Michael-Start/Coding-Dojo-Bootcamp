d910cf7a0ccebaed09b69a0e4ac9920e9757032b
Michael Start <103297119+Michael-Start@users.noreply.github.com>
2022-06-27 14:02:47 -0700

    6/27/22

---
+++ Python/flask/fundamentals/checkerboard/server.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html', x = 4)
+
+@app.route('/<int:x>')
+def vartable(x):
+
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
